01854139db9df29a5754d7b42cc2ae5b4c07313c
sixmillions <liubw95@163.com>
2023-05-16 11:39:38 +0000

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="{'dark': darkMode}">
+    <h1 class="text-red-500 dark:text-dim-500">Home Page</h1>
+  </div>
+</template>
+
+<script setup>
+const darkMode = ref(true)
+</script>
