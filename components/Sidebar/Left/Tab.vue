86b2ec07861eb533c3ca78b4f80aa9e969a527ee
sixmillions <liubw95@163.com>
2023-05-17 12:01:57 +0000

    左侧导航栏

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,39 @@
+<template>
+  <!--
+    flex: 使用 flex 布局；
+    items-center: 将子元素在主轴方向上居中对齐；
+    p-3: 设置 padding 为 3；
+    w-min: 设置元素的宽度为最小宽度；
+    text-black: 文本颜色为黑色；
+    hover:bg-gray-300: 鼠标悬停时背景颜色为灰色；
+    space-x-4: 同一元素组内子元素之间的横向间距为 4；
+    rounded-full: 圆角为满圆；
+    dark:hover:bg-dim-200: 在暗黑模式下，鼠标悬停时背景颜色为灰暗色；
+    dark:text-white: 在暗黑模式下，文本颜色为白色。
+   -->
+  <NuxtLink to="#" class="flex items-center p-3 w-min text-black rounded-full hover:bg-gray-300 space-x-4 dark:hover:bg-dim-200 dark:text-white"
+    :class="defaultTransition">
+    <div class="w-6 h-6" :class="defaultTransition">
+      <slot name="icon"></slot>
+    </div>
+    <!--
+      默认隐藏，在xl大小下显示
+     -->
+    <div class="hidden text-xl xl:block" :class="textClass">
+      <slot name="name"></slot>
+    </div>
+  </NuxtLink>
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig()
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false
+  }
+})
+
+// 凸显激活状态
+const textClass = computed(() => props.active ? 'font-semibold' : 'font-normal')
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <!--
+      p-2 将内边距设置为2。
+      my-2 将上下外边距设置为2。
+      w-min 将宽度设置为最小内容宽度。也就是跟着内容变化，这里内容设置为w-8，并且这个元素设置为p2 my-2，所以这个div是w-12，也就是48px
+      hover:bg-blue-500 将背景颜色设置为蓝色-500当元素悬停时。
+      rounded-full 将边框半径设置为满圆形。
+      dark:hover:bg-white/20 在暗模式环境中，将背景颜色设置为白色，透明度为20%，当元素悬停时。
+    -->
+    <div class="p-2 my-2 w-min rounded-full hover:bg-blue-500 dark:hover:bg-white/20" :class="defaultTransition">
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+
+    <div class=" mt-2 space-y-3">
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name>
+          Home
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name>
+          Explore
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name>
+          Notifications
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name>
+          Messages
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BookmarkIcon />
+        </template>
+        <template v-slot:name>
+          Bookmarks
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <DocumentIcon />
+        </template>
+        <template v-slot:name>
+          Lists
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name>
+          Profile
+        </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <EllipsisHorizontalCircleIcon />
+        </template>
+        <template v-slot:name>
+          Mores
+        </template>
+      </SidebarLeftTab>
+    </div>
+  </div>
+</template>
+<script setup>
+import { HomeIcon } from '@heroicons/vue/24/solid'
+import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentIcon, UserIcon, EllipsisHorizontalCircleIcon } from '@heroicons/vue/24/outline'
+const { defaultTransition } = useTailwindConfig()
+</script>
