721aad585a2c2684bc6742049006997b9ee01dd7
sixmillions <liubw95@163.com>
2023-05-24 22:12:48 +0800

    发表Tweet；上传文件

---
+++ components/Tweet/Form/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <TweetFormInput :user="props.user" @onSubmit="handleFormSubmit" />
+  </div>
+</template>
+<script setup>
+const { postTweet } = useTweet()
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true
+  }
+})
+
+async function handleFormSubmit(data) {
+  try {
+    const res = await postTweet({
+      text: data.text,
+      mediaFiles: data.mediaFiles
+    })
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
     <MainSection title="Home" :loading="loading">
-      Main Content
-      <div>{{ token }}</div>
-      <div>{{ user }}</div>
-      <button @click="refreshCnt">刷新</button>
+      <TweetForm :user="user"/>
     </MainSection>
   </div>
 </template>
 <script setup>
 const loading = ref(false)
-const { useAuthToken, useAuthUser } = useAuth()
+const { useAuthUser } = useAuth()
 
-const token = ref('')
-const user = ref({})
-const refreshCnt = () => {
-  token.value = useAuthToken().value
-  user.value = useAuthUser().value
-}
+const user = useAuthUser()
 
 </script>
